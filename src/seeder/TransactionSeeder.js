315f171b2fa340dfac586ed87cae6c3c612811a8
Yigit Gulay <80751083+yigdo@users.noreply.github.com>
2023-03-26 20:34:57 +0300

    Added a recrusive function to practice

--- src/seeder/TransactionSeeder.js
+++ src/seeder/TransactionSeeder.js
@@ -1,16 +1,16 @@
 const { randomName, randomSurname, RandomInt } = require("../data/DummyData");
 
-function TransactionSeeder(seedAmount){
+function TransactionSeeder(seedAmount) {
     let insertArray = []
     let workingEntity;
     for (let i = 0; i < seedAmount; i++) {
-        workingEntity = {name: randomName(), surname: randomSurname(), tcNumber:RandomInt(10000000000, 99999999999), _transactionNumber: RandomInt(0,10000000)}
-        
+        workingEntity = { name: randomName(), surname: randomSurname(), tcNumber: RandomInt(10000000000, 99999999999), _transactionNumber: RandomInt(0, seedAmount+1) }
+
         do {
 
-            if(insertArray.find(e => e._transactionNumber == workingEntity._transactionNumber) == null){
+            if (insertArray.find(e => e._transactionNumber == workingEntity._transactionNumber) == null) {
                 isOk = true;
-            }else{
+            } else {
                 isOk = false;
             }
 
@@ -19,5 +19,12 @@ function TransactionSeeder(seedAmount){
     }
     return insertArray;
 }
+function createSingleRecord(name, surname, tcNumber, allEntities) {
+    let workingEntity = {name: name, surname: surname, tcNumber: tcNumber, _transactionNumber: RandomInt(0,10000000)}
 
+    if(allEntities.find(e => e._transactionNumber == workingEntity._transactionNumber) != null){
+        createSingleRecord(name, surname, tcNumber, allEntities);
+    }
+    return workingEntity;
+}
 exports.TransactionSeeder = TransactionSeeder
